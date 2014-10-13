be6b62d001f1e73a4245e608e712d30cdd68bfdb
Daniel Moran <dmj@tid.es>
2014-10-13 12:38:16 +0200

    ADD Multiple device context registration

--- test/unit/fiware-iotagent-lib_test.js
+++ test/unit/fiware-iotagent-lib_test.js
@@ -28,6 +28,16 @@ var iotAgentLib = require('../../'),
                 type: 'Percentage'
             }
         ]
+    },
+    device2 = {
+        id: 'term2',
+        type: 'Termometer',
+        attributes: [
+            {
+                name: 'temperature',
+                type: 'Centigrades'
+            }
+        ]
     };
 
 describe('IoT Agent NGSI Integration', function() {
@@ -57,8 +67,8 @@ describe('IoT Agent NGSI Integration', function() {
 
     describe('When a new device is connected to the IoT Agent', function() {
         beforeEach(function(done) {
-            var expectedPayload = utils
-                .readExampleFile('./test/unit/contextAvailabilityRequests/registerNewDevice1.json');
+            var expectedPayload1 = utils
+                    .readExampleFile('./test/unit/contextAvailabilityRequests/registerNewDevice1.json');
 
             nock.cleanAll();
 
@@ -71,10 +81,10 @@ describe('IoT Agent NGSI Integration', function() {
                     utils.readExampleFile('./test/unit/contextAvailabilityResponses/registerIoTAgent1Success.json'));
 
             iotAgentLib.activate(iotAgentConfig, function(error) {
-                expectedPayload.registrationId = iotAgentLib.getRegistrationId();
+                expectedPayload1.registrationId = iotAgentLib.getRegistrationId();
 
                 contextBrokerMock
-                    .post('/NGSI9/registerContext', expectedPayload)
+                    .post('/NGSI9/registerContext', expectedPayload1)
                     .reply(200, utils.readExampleFile(
                         './test/unit/contextAvailabilityResponses/registerNewDevice1Success.json'));
 
@@ -90,10 +100,53 @@ describe('IoT Agent NGSI Integration', function() {
             });
         });
     });
+
+    describe('When a new device is connected to an IoT Agent with more devices', function() {
+        beforeEach(function(done) {
+            var expectedPayload1 = utils
+                    .readExampleFile('./test/unit/contextAvailabilityRequests/registerNewDevice1.json'),
+                expectedPayload2 = utils
+                    .readExampleFile('./test/unit/contextAvailabilityRequests/registerNewDevice2.json');
+
+            nock.cleanAll();
+
+            contextBrokerMock = nock('http://10.11.128.16:1026')
+                .matchHeader('fiware-service', 'smartGondor')
+                .matchHeader('fiware-servicepath', 'gardens')
+                .post('/NGSI9/registerContext',
+                    utils.readExampleFile('./test/unit/contextAvailabilityRequests/registerIoTAgent1.json'))
+                .reply(200,
+                    utils.readExampleFile('./test/unit/contextAvailabilityResponses/registerIoTAgent1Success.json'));
+
+            iotAgentLib.activate(iotAgentConfig, function(error) {
+                expectedPayload1.registrationId = iotAgentLib.getRegistrationId();
+                expectedPayload2.registrationId = iotAgentLib.getRegistrationId();
+
+                contextBrokerMock
+                    .post('/NGSI9/registerContext', expectedPayload1)
+                    .reply(200, utils.readExampleFile(
+                        './test/unit/contextAvailabilityResponses/registerNewDevice1Success.json'));
+
+                contextBrokerMock
+                    .post('/NGSI9/registerContext', expectedPayload2)
+                    .reply(200, utils.readExampleFile(
+                        './test/unit/contextAvailabilityResponses/registerNewDevice2Success.json'));
+
+                iotAgentLib.register(device1.id, device1.type, device1.attributes, done);
+            });
+        });
+
+        it('should update the IoT Agent registration with all the devices', function(done) {
+            iotAgentLib.register(device2.id, device2.type, device2.attributes, function(error) {
+                should.not.exist(error);
+                contextBrokerMock.done();
+                done();
+            });
+        });
+    });
+
     describe('When a device is removed from the IoT Agent', function() {
-        it('should cancel its registration in the Context Broker');
-        it('should cancel data update modifications related to the device');
-        it('should send the appropriate service headers');
+        it('should update the devices information in the Context Broker');
     });
     describe('When the IoT Agent receives new information from a device', function() {
         beforeEach(function () {
