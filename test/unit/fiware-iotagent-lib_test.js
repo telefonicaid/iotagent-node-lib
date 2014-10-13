63db20b9606ae6001da8f6e7f0d4859c1f9e9bf8
Daniel Moran <dmj@tid.es>
2014-10-13 11:08:50 +0200

    ADD Register IoT Agent on startup

--- test/unit/fiware-iotagent-lib_test.js
+++ test/unit/fiware-iotagent-lib_test.js
@@ -31,19 +31,55 @@ var iotAgentLib = require('../../'),
     };
 
 describe('IoT Agent NGSI Integration', function() {
-    describe.only('When a new device is connected to the IoT Agent', function() {
+    describe('When an IoT Agent is started', function() {
         beforeEach(function(done) {
+            nock.cleanAll();
+
             contextBrokerMock = nock('http://10.11.128.16:1026')
                 .post('/NGSI9/registerContext',
-                    utils.readExampleFile('./test/unit/contextAvailabilityRequests/registerNewDevice1.json'))
+                    utils.readExampleFile('./test/unit/contextAvailabilityRequests/registerIoTAgent1.json'))
                 .reply(200,
-                    utils.readExampleFile('./test/unit/contextAvailabilityResponses/registerNewDevice1Success.json'));
+                    utils.readExampleFile('./test/unit/contextAvailabilityResponses/registerIoTAgent1Success.json'));
 
-            iotAgentLib.activate(iotAgentConfig, done);
+            done();
+        });
+
+        it('should register itself in the contextBroker, and save the returned ID', function(done) {
+            iotAgentLib.activate(iotAgentConfig, function(error) {
+                should.not.exist(error);
+                iotAgentLib.getRegistrationId().should.equal('6319a7f5254b05844116584d');
+                done();
+            });
+        });
+    });
+
+    describe('When a new device is connected to the IoT Agent', function() {
+        beforeEach(function(done) {
+            var expectedPayload = utils
+                .readExampleFile('./test/unit/contextAvailabilityRequests/registerNewDevice1.json');
+
+            nock.cleanAll();
+
+            contextBrokerMock = nock('http://10.11.128.16:1026')
+                .post('/NGSI9/registerContext',
+                    utils.readExampleFile('./test/unit/contextAvailabilityRequests/registerIoTAgent1.json'))
+                .reply(200,
+                    utils.readExampleFile('./test/unit/contextAvailabilityResponses/registerIoTAgent1Success.json'));
+
+            iotAgentLib.activate(iotAgentConfig, function(error) {
+                expectedPayload.registrationId = iotAgentLib.getRegistrationId();
+
+                contextBrokerMock
+                    .post('/NGSI9/registerContext', expectedPayload)
+                    .reply(200, utils.readExampleFile(
+                        './test/unit/contextAvailabilityResponses/registerNewDevice1Success.json'));
+
+                done();
+            });
         });
 
         it('should register the device context in the Context Broker', function(done) {
-            iotAgentLib.register(device1.id, device1.type, device1.attributes, function (error) {
+            iotAgentLib.register(device1.id, device1.type, device1.attributes, function(error) {
                 should.not.exist(error);
                 contextBrokerMock.done();
                 done();
@@ -60,7 +96,7 @@ describe('IoT Agent NGSI Integration', function() {
     describe('When the IoT Agent receives new information from a device', function() {
         it('should change the value of the corresponding attribute in the context broker');
     });
-    describe('When the IoT Agent receives an update on the device data', function () {
+    describe('When the IoT Agent receives an update on the device data', function() {
         it('should call the device handler with the received data');
     });
     describe('When a context query arrives to the IoT Agent', function() {
