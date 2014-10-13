3dc36cdfa7177003809c28b123bf9d3613ae5b93
Daniel Moran <dmj@tid.es>
2014-10-13 11:09:17 +0200

    FIX Add missing request payloads

---
+++ test/tools/utils.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2013 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-orion-pep
+ *
+ * fiware-orion-pep is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * fiware-orion-pep is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with fiware-orion-pep.
+ * If not, seehttp://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License
+ * please contact with::[daniel.moranjimenez@telefonica.com]
+ */
+
+'use strict';
+
+var fs = require('fs');
+
+function readExampleFile(name, raw) {
+    var text = fs.readFileSync(name, 'UTF8');
+
+    if (raw) {
+        return text;
+    } else {
+        return JSON.parse(text);
+    }
+}
+
+exports.readExampleFile = readExampleFile;
