3bb89cedc26e20adf228f17295810a8e8ed13f57
Daniel Moran <dmj@tid.es>
2015-01-21 17:14:50 +0100

    FIX Linter errors

--- test/unit/device-provisioning-api_test.js
+++ test/unit/device-provisioning-api_test.js
@@ -107,7 +107,7 @@ describe('Device provisioning API', function() {
                     results[0].staticAttributes[0].name.should.equal('attr_name');
                     should.exist(results[0].internalAttributes);
                     results[0].internalAttributes.length.should.equal(1);
-                    results[0].internalAttributes[0]['customField'].should.equal('customValue');
+                    results[0].internalAttributes[0].customField.should.equal('customValue');
                     done();
                 });
             });
