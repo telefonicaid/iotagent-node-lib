33a757ceba184fbe327fa9dbd15abab35ea20a94
Daniel Moran <dmj@tid.es>
2015-02-05 16:31:50 -0200

    ADD Get single device

---
+++ test/unit/removeProvisionedDevice-test.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2014 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-iotagent-lib
+ *
+ * fiware-iotagent-lib is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * fiware-iotagent-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with fiware-iotagent-lib.
+ * If not, seehttp://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License
+ * please contact with::[contacto@tid.es]
+ */
+'use strict';
+
+describe('Device provisioning API: Remove provisioned devices', function() {
+    describe('When a request to remove a provision device arrives', function() {
+        it('should remove the device from the provisioned devices list');
+        it('should return a 404 error when asking for the particular device');
+    });
+});
