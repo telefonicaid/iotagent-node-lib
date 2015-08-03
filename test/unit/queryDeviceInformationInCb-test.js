9bda53cb1bbee3e84b969160dec5e16c2752d680
Daniel Moran <dmj@tid.es>
2015-08-03 17:11:16 +0200

    ADD Query context operations over the device

---
+++ test/unit/queryDeviceInformationInCb-test.js
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2014 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-iotagent-lib
+ *
+ * fiware-iotagent-lib is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * fiware-iotagent-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with fiware-iotagent-lib.
+ * If not, seehttp://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License
+ * please contact with::[contacto@tid.es]
+ */
+'use strict';
+
+var iotAgentLib = require('../../'),
+    utils = require('../tools/utils'),
+    should = require('should'),
+    logger = require('fiware-node-logger'),
+    nock = require('nock'),
+    contextBrokerMock,
+    iotAgentConfig = {
+        contextBroker: {
+            host: '10.11.128.16',
+            port: '1026'
+        },
+        server: {
+            port: 4041
+        },
+        types: {
+            'Light': {
+                commands: [],
+                type: 'Light',
+                lazy: [
+                    {
+                        name: 'temperature',
+                        type: 'centigrades'
+                    }
+                ],
+                active: [
+                    {
+                        name: 'pressure',
+                        type: 'Hgmm'
+                    }
+                ]
+            },
+            'BrokenLight': {
+                commands: [],
+                lazy: [
+                    {
+                        name: 'temperature',
+                        type: 'centigrades'
+                    }
+                ],
+                active: [
+                    {
+                        name: 'pressure',
+                        type: 'Hgmm'
+                    }
+                ]
+            },
+            'Termometer': {
+                type: 'Termometer',
+                commands: [],
+                lazy: [
+                    {
+                        name: 'temp',
+                        type: 'kelvin'
+                    }
+                ],
+                active: [
+                ]
+            },
+            'Humidity': {
+                type: 'Humidity',
+                cbHost: 'http://192.168.1.1:3024',
+                commands: [],
+                lazy: [],
+                active: [
+                    {
+                        name: 'humidity',
+                        type: 'percentage'
+                    }
+                ]
+            },
+            'Motion': {
+                type: 'Motion',
+                commands: [],
+                lazy: [],
+                staticAttributes: [
+                    {
+                        'name': 'location',
+                        'type': 'Vector',
+                        'value': '(123,523)'
+                    }
+                ],
+                active: [
+                    {
+                        name: 'humidity',
+                        type: 'percentage'
+                    }
+                ]
+            }
+        },
+        service: 'smartGondor',
+        subservice: 'gardens',
+        providerUrl: 'http://smartGondor.com',
+        deviceRegistrationDuration: 'P1M',
+        throttling: 'PT5S'
+    };
+
+describe.only('Query device information in the Context Broker', function() {
+    var attributes = [
+        'state',
+        'dimming'
+    ];
+
+    beforeEach(function(done) {
+        logger.setLevel('FATAL');
+        nock.cleanAll();
+
+        contextBrokerMock = nock('http://10.11.128.16:1026')
+            .matchHeader('fiware-service', 'smartGondor')
+            .matchHeader('fiware-servicepath', 'gardens')
+            .post('/v1/queryContextQ',
+            utils.readExampleFile('./test/unit/contextRequests/queryContext1.json'))
+            .reply(200,
+            utils.readExampleFile('./test/unit/contextResponses/queryContext1Success.json'));
+
+        iotAgentLib.activate(iotAgentConfig, done);
+    });
+
+    afterEach(function(done) {
+        iotAgentLib.deactivate(done);
+    });
+
+    describe('When the user requests information about a registered device', function() {
+        it('should return the information about the desired attributes', function(done) {
+            iotAgentLib.query('light1', 'Light', '', attributes, function(error) {
+                should.not.exist(error);
+                contextBrokerMock.done();
+                done();
+            });
+        });
+    });
+
+    describe('When the user requests information about a device that it\'s not in the CB', function() {
+        it('should return a DEVICE_NOT_FOUND_ERROR');
+    });
+
+    describe('When the user requests information about a device in a protected CB', function() {
+        it('should send the Auth Token along with the information query');
+    });
+});
