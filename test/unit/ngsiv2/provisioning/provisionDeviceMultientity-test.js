690241fa48ed35617a3711765ae0b5d0236e7409
Daniel Calvo <daniel.calvo@atos.net>
2018-06-28 14:21:46 +0200

    Adds new unit tests #637

---
+++ test/unit/ngsiv2/provisioning/provisionDeviceMultientity-test.js
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2014 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-iotagent-lib
+ *
+ * fiware-iotagent-lib is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * fiware-iotagent-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with fiware-iotagent-lib.
+ * If not, seehttp://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License
+ * please contact with::[contacto@tid.es]
+ *
+ * Modified by: Daniel Calvo - ATOS Research & Innovation
+ */
+'use strict';
+
+var iotAgentLib = require('../../../../lib/fiware-iotagent-lib'),
+    utils = require('../../../tools/utils'),
+
+    should = require('should'),
+    nock = require('nock'),
+    moment = require('moment'),
+    request = require('request'),
+    contextBrokerMock,
+    iotAgentConfig = {
+        logLevel: 'DEBUG',
+        contextBroker: {
+            host: '192.168.1.1',
+            port: '1026',
+            ngsiVersion: 'v2'
+        },
+        server: {
+            port: 4041,
+            baseRoot: '/'
+        },
+        types: {},
+        service: 'smartGondor',
+        subservice: 'gardens',
+        providerUrl: 'http://smartGondor.com',
+        deviceRegistrationDuration: 'P1M',
+        throttling: 'PT5S'
+    };
+
+describe('Device provisioning API: Provision devices', function() {
+    beforeEach(function(done) {
+        nock.cleanAll();
+
+        iotAgentLib.activate(iotAgentConfig, function() {
+            iotAgentLib.clearAll(done);
+        });
+    });
+
+    afterEach(function(done) {
+        nock.cleanAll();
+        iotAgentLib.setProvisioningHandler();
+        iotAgentLib.deactivate(done);
+    });
+
+    describe('When a device provisioning request with all the required data arrives to the IoT Agent', function() {
+        beforeEach(function() {
+            nock.cleanAll();
+
+            contextBrokerMock = nock('http://192.168.1.1:1026')
+                .matchHeader('fiware-service', 'smartGondor')
+                .matchHeader('fiware-servicepath', '/gardens')
+                .post('/v2/registrations', function(body) {
+                    var expectedBody = utils.readExampleFile('./test/unit/ngsiv2/examples' +
+                        '/contextAvailabilityRequests/registerProvisionedDevice.json');
+
+                    // Note that expired field is not included in the json used by this mock as it is a dynamic
+                    // field. The following code performs such calculation and adds the field to the subscription
+                    // payload of the mock.
+                    if (!body.expires)
+                    {
+                        return false;
+                    }
+                    else if (moment(body.expires, 'YYYY-MM-DDTHH:mm:ss.SSSZ').isValid())
+                    {
+                        expectedBody.expires = moment().add(moment.duration(iotAgentConfig.deviceRegistrationDuration));
+                        var expiresDiff = moment(expectedBody.expires).diff(body.expires, 'milliseconds');
+                        if (expiresDiff < 500) {
+                            delete expectedBody.expires;
+                            delete body.expires;
+
+                            return JSON.stringify(body) === JSON.stringify(expectedBody);
+                        }
+
+                        return false;
+                    }
+                    else {
+                        return false;
+                    }
+                })
+                .reply(201, null, {'Location': '/v2/registrations/6319a7f5254b05844116584d'});
+
+            contextBrokerMock
+                .matchHeader('fiware-service', 'smartGondor')
+                .matchHeader('fiware-servicepath', '/gardens')
+                .post('/v2/entities?options=upsert', utils.readExampleFile(
+                    './test/unit/ngsiv2/examples/contextRequests/createProvisionedDeviceMultientity.json'))
+                .reply(204);
+        });
+
+        var options = {
+            url: 'http://localhost:' + iotAgentConfig.server.port + '/iot/devices',
+            method: 'POST',
+            json: utils.readExampleFile('./test/unit/examples/' +
+                'deviceProvisioningRequests/provisionNewDeviceMultientity.json'),
+            headers: {
+                'fiware-service': 'smartGondor',
+                'fiware-servicepath': '/gardens'
+            }
+        };
+
+        it('should add the device to the devices list', function(done) {
+            request(options, function(error, response, body) {
+                should.not.exist(error);
+                response.statusCode.should.equal(201);
+
+                iotAgentLib.listDevices('smartGondor', '/gardens', function(error, results) {
+                    results.devices.length.should.equal(1);
+                    done();
+                });
+            });
+        });
+
+    });
+});
+
+
