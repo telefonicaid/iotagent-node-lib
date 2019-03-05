83b32a199751ca31ab94479172044d78755cb1dd
a620381 <daniel.calvo@atos.net>
2019-03-05 09:30:38 +0100

    fix(https support): finish addressing PR #594 comments

--- test/unit/general/https-support-test.js
+++ test/unit/general/https-support-test.js
@@ -19,6 +19,9 @@
  *
  * For those usages not covered by the GNU Affero General Public License
  * please contact with::[contacto@tid.es]
+ *
+ * Modified by: Federico M. Facca - Martel Innovate
+ * Modified by: Daniel Calvo - ATOS Research & Innovation
  */
 'use strict';
 
@@ -30,7 +33,6 @@ var iotAgentLib = require('../../../lib/fiware-iotagent-lib'),
     utils = require('../../tools/utils'),
     groupRegistryMemory = require('../../../lib/services/groups/groupRegistryMemory'),
     should = require('should'),
-    moment = require('moment'),
     iotAgentConfig = {
         logLevel: 'FATAL',
         contextBroker: {
@@ -85,53 +87,6 @@ var iotAgentLib = require('../../../lib/fiware-iotagent-lib'),
         },
         defaultResource: '/iot/d'
     },
-    iotAgentConfigV2 = {
-        logLevel: 'FATAL',
-        contextBroker: {
-            url: 'https://192.168.1.1:1026',
-            ngsiVersion: 'v2'
-        },
-        server: {
-            port: 4041
-        },
-        types: {
-            'Light': {
-                commands: [],
-                lazy: [
-                    {
-                        name: 'temperature',
-                        type: 'centigrades'
-                    }
-                ],
-                active: [
-                    {
-                        name: 'pressure',
-                        type: 'Hgmm'
-                    }
-                ],
-                service: 'smartGondor',
-                subservice: 'gardens'
-            },
-            'Termometer': {
-                commands: [],
-                lazy: [
-                    {
-                        name: 'temp',
-                        type: 'kelvin'
-                    }
-                ],
-                active: [
-                ],
-                service: 'smartGondor',
-                subservice: 'gardens'
-            }
-        },
-        service: 'smartGondor',
-        subservice: 'gardens',
-        providerUrl: 'http://smartGondor.com',
-        deviceRegistrationDuration: 'P1M',
-        throttling: 'PT5S'
-    },
     groupCreation = {
         service: 'theService',
         subservice: 'theSubService',
@@ -176,7 +131,7 @@ describe('HTTPS support tests IOTAM', function() {
 
             iotamMock = nock('https://mockediotam.com:9876')
                 .post('/protocols',
-                utils.readExampleFile('./test/unit/examples/iotamRequests/registrationWithGroups.json'))
+                utils.readExampleFile('./test/unit/examples/iotamRequests/registrationWithGroupsWithoutCB.json'))
                 .reply(200,
                 utils.readExampleFile('./test/unit/examples/iotamResponses/registrationSuccess.json'));
 
@@ -200,7 +155,7 @@ describe('HTTPS support tests IOTAM', function() {
     });
 });
 
-describe('HTTPS support tests NGSIv1', function() {
+describe('HTTPS support tests', function() {
 
     describe('When subscription is sent to HTTPS context broker', function() {
         beforeEach(function(done) {
@@ -312,140 +267,5 @@ describe('HTTPS support tests NGSIv1', function() {
           });
         });
 
-    });
-});
-
-describe('HTTPS support tests NGSIv2', function() {
-
-    describe('When subscription is sent to HTTPS context broker', function() {
-        beforeEach(function(done) {
-            var optionsProvision = {
-                url: 'http://localhost:' + iotAgentConfig.server.port + '/iot/devices',
-                method: 'POST',
-                json: utils.readExampleFile(
-                    './test/unit/examples/deviceProvisioningRequests/provisionMinimumDevice.json'),
-                headers: {
-                    'fiware-service': 'smartGondor',
-                    'fiware-servicepath': '/gardens'
-                }
-            };
-
-            nock.cleanAll();
-
-            iotAgentLib.activate(iotAgentConfigV2, function() {
-                contextBrokerMock = nock('https://192.168.1.1:1026')
-                    .matchHeader('fiware-service', 'smartGondor')
-                    .matchHeader('fiware-servicepath', '/gardens')
-                    .post('/v2/entities?options=upsert',
-                        utils.readExampleFile('./test/unit/ngsiv2/examples/' +
-                            'contextRequests/createMinimumProvisionedDevice.json'))
-                    .reply(204);
-
-                contextBrokerMock = nock('https://192.168.1.1:1026')
-                    .matchHeader('fiware-service', 'smartGondor')
-                    .matchHeader('fiware-servicepath', '/gardens')
-                    .post('/v2/subscriptions', function(body) {
-                        var expectedBody = utils.readExampleFile('./test/unit/ngsiv2/examples' +
-                            '/subscriptionRequests/simpleSubscriptionRequest.json');
-                        // Note that expired field is not included in the json used by this mock as it is a dynamic
-                        // field. The following code performs such calculation and adds the field to the subscription
-                        // payload of the mock.
-                        if (!body.expires)
-                        {
-                            return false;
-                        }
-                        else if (moment(body.expires, 'YYYY-MM-DDTHH:mm:ss.SSSZ').isValid())
-                        {
-                            expectedBody.expires = moment().add(moment.duration(iotAgentConfig.deviceRegistrationDuration));
-                            var expiresDiff = moment(expectedBody.expires).diff(body.expires, 'milliseconds');
-                            if (expiresDiff < 500) {
-                                delete expectedBody.expires;
-                                delete body.expires;
-
-                                return JSON.stringify(body) === JSON.stringify(expectedBody);
-                            }
-
-                            return false;
-                        }
-                        else {
-                            return false;
-                        }
-                    })
-                    .reply(201, null, {'Location': '/v2/subscriptions/51c0ac9ed714fb3b37d7d5a8'});
-
-
-                iotAgentLib.clearAll(function() {
-                    request(optionsProvision, function(error, result, body) {
-                        done();
-                    });
-                });
-            });
-        });
-
-        afterEach(function(done) {
-            nock.cleanAll();
-            iotAgentLib.setNotificationHandler();
-            iotAgentLib.clearAll(function() {
-                iotAgentLib.deactivate(done);
-            });
-        });
-
-        it('should send the appropriate request to the Context Broker', function(done) {
-            iotAgentLib.getDevice('MicroLight1', 'smartGondor', '/gardens', function(error, device) {
-                iotAgentLib.subscribe(device, ['attr_name'], null, function(error) {
-                    should.not.exist(error);
-
-                    contextBrokerMock.done();
-
-                    done();
-                });
-            });
-        });
-    });
-
-    describe('When a new device is connected to the IoT Agent', function() {
-        beforeEach(function(done) {
-            nock.cleanAll();
-
-            // This mock does not check the payload since the aim of the test is not to verify
-            // device provisioning functionality. Appropriate verification is done in tests under
-            // provisioning folder
-            contextBrokerMock = nock('https://192.168.1.1:1026')
-                .matchHeader('fiware-service', 'smartGondor')
-                .matchHeader('fiware-servicepath', 'gardens')
-                .post('/v2/entities?options=upsert')
-                .reply(204);
-
-            var nockBody = utils.readExampleFile(
-                './test/unit/ngsiv2/examples/contextAvailabilityRequests/registerIoTAgent1.json');
-            nockBody.expires = /.+/i;
-            contextBrokerMock
-                .matchHeader('fiware-service', 'smartGondor')
-                .matchHeader('fiware-servicepath', 'gardens')
-                .post('/v2/registrations', nockBody)
-                .reply(201, null, {'Location': '/v2/registrations/6319a7f5254b05844116584d'});
-
-
-            iotAgentLib.activate(iotAgentConfigV2, function(error) {
-                iotAgentLib.clearAll(done);
-            });
-        });
-
-        it('should register as ContextProvider using HTTPS', function(done) {
-            iotAgentLib.register(device1, function(error) {
-                    should.not.exist(error);
-                    contextBrokerMock.done();
-                    done();
-            });
-        });
-
-        afterEach(function(done) {
-            nock.cleanAll();
-            iotAgentLib.clearAll(function() {
-                // We need to remove the registrationId so that the library does not consider next operatios as updates.
-                delete device1.registrationId;
-                iotAgentLib.deactivate(done);
-            });
-        });
     });
 });
