dc3e21fb91cdfe392e198406ab1df8fbc34f78d3
Jagatjot Singh <jagatjotsingh22@gmail.com>
2019-04-17 12:04:27 +0530

    Update mongodb-group-registry-test.js

--- test/unit/mongodb/mongodb-group-registry-test.js
+++ test/unit/mongodb/mongodb-group-registry-test.js
@@ -102,6 +102,48 @@ var iotAgentLib = require('../../../lib/fiware-iotagent-lib'),
             'fiware-servicepath': '/testingPath'
         }
     },
+    optionsCreationcgroups = {
+        url: 'http://localhost:4041/iot/cgroups',
+        method: 'POST',
+        json: {
+            services: [
+                {
+                    resource: '/deviceTest',
+                    apikey: '801230BJKL23Y9090DSFL123HJK09H324HV8732',
+                    entity_type: 'Light',
+                    trust: '8970A9078A803H3BL98PINEQRW8342HBAMS',
+                    cbHost: 'http://unexistentHost:1026',
+                    commands: [
+                        {
+                            name: 'wheel1',
+                            type: 'Wheel'
+                        }
+                    ],
+                    lazy: [
+                        {
+                            name: 'luminescence',
+                            type: 'Lumens'
+                        }
+                    ],
+                    attributes: [
+                        {
+                            name: 'status',
+                            type: 'Boolean'
+                        }
+                    ],
+                    internal_attributes: [
+                        {
+                            customField: 'customValue'
+                        }
+                    ]
+                }
+            ]
+        },
+        headers: {
+            'fiware-service': 'TestService',
+            'fiware-servicepath': '/testingPath'
+        }
+    },
     optionsDelete = {
         url: 'http://localhost:4041/iot/services',
         method: 'DELETE',
@@ -115,8 +157,30 @@ var iotAgentLib = require('../../../lib/fiware-iotagent-lib'),
             apikey: '801230BJKL23Y9090DSFL123HJK09H324HV8732'
         }
     },
+    optionsDeletecgroups = {
+        url: 'http://localhost:4041/iot/cgroups',
+        method: 'DELETE',
+        json: {},
+        headers: {
+            'fiware-service': 'TestService',
+            'fiware-servicepath': '/testingPath'
+        },
+        qs: {
+            resource: '/deviceTest',
+            apikey: '801230BJKL23Y9090DSFL123HJK09H324HV8732'
+        }
+    },
     optionsList = {
         url: 'http://localhost:4041/iot/services',
+        method: 'GET',
+        json: {},
+        headers: {
+            'fiware-service': 'TestService',
+            'fiware-servicepath': '/*'
+        }
+    },
+    optionsListcgroups = {
+        url: 'http://localhost:4041/iot/cgroups',
         method: 'GET',
         json: {},
         headers: {
@@ -166,8 +230,59 @@ var iotAgentLib = require('../../../lib/fiware-iotagent-lib'),
             apikey: '801230BJKL23Y9090DSFL123HJK09H324HV8732'
         }
     },
+    optionsUpdatecgroups = {
+        url: 'http://localhost:4041/iot/cgroups',
+        method: 'PUT',
+        json: {
+            apikey: '801230BJKL23Y9090DSFL123HJK09H324HV8732',
+            trust: '8970A9078A803H3BL98PINEQRW8342HBAMS',
+            cbHost: 'http://anotherUnexistentHost:1026',
+            commands: [
+                {
+                    name: 'wheel1',
+                    type: 'Wheel'
+                }
+            ],
+            lazy: [
+                {
+                    name: 'luminescence',
+                    type: 'Lumens'
+                }
+            ],
+            attributes: [
+                {
+                    name: 'status',
+                    type: 'Boolean'
+                }
+            ],
+            static_attributes: [
+                {
+                    name: 'bootstrapServer',
+                    type: 'Address',
+                    value: '127.0.0.1'
+                }
+            ]
+        },
+        headers: {
+            'fiware-service': 'TestService',
+            'fiware-servicepath': '/testingPath'
+        },
+        qs: {
+            resource: '/deviceTest',
+            apikey: '801230BJKL23Y9090DSFL123HJK09H324HV8732'
+        }
+    },
     optionsGet = {
         url: 'http://localhost:4041/iot/services',
+        method: 'GET',
+        json: {},
+        headers: {
+            'fiware-service': 'TestService',
+            'fiware-servicepath': '/testingPath'
+        }
+    },
+    optionsGetcgroups = {
+        url: 'http://localhost:4041/iot/cgroups',
         method: 'GET',
         json: {},
         headers: {
@@ -347,6 +462,19 @@ describe('MongoDB Group Registry test', function() {
     describe('When a device group listing arrives with a limit', function() {
         var optionsConstrained = {
             url: 'http://localhost:4041/iot/services',
+            method: 'GET',
+            qs: {
+                limit: 3,
+                offset: 2
+            },
+            json: {},
+            headers: {
+                'fiware-service': 'TestService',
+                'fiware-servicepath': '/*'
+            }
+        },
+        optionsConstrainedcgroups = {
+            url: 'http://localhost:4041/iot/cgroups',
             method: 'GET',
             qs: {
                 limit: 3,
