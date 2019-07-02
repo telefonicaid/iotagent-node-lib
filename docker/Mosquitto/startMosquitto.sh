681dc36648769291ded2f2b767794b9e06105f32
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2019-07-02 11:39:02 +0200

    print human readable timestamp

--- docker/Mosquitto/startMosquitto.sh
+++ docker/Mosquitto/startMosquitto.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 mosquitto_passwd -b /etc/mosquitto/pwfile iota ${IOTA_PASS}
-/usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf
+/usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf &
+tail -f /var/log/mosquitto/mosquitto.log | perl -pe 's/(\d+)/localtime($1)/e'
