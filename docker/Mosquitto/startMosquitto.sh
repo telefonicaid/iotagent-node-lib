28cfb0e7d68ca06a4b8bf978d8c6c2ed5ea778a4
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2019-07-03 12:24:19 +0200

    fix log permissions

--- docker/Mosquitto/startMosquitto.sh
+++ docker/Mosquitto/startMosquitto.sh
@@ -3,4 +3,5 @@
 mosquitto_passwd -b /etc/mosquitto/pwfile iota ${IOTA_PASS}
 /usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf &
 touch /var/log/mosquitto/mosquitto.log
+chmod 664 /var/log/mosquitto/mosquitto.log
 tail -f /var/log/mosquitto/mosquitto.log | perl -pe 's/(\d+)/localtime($1)/e'
