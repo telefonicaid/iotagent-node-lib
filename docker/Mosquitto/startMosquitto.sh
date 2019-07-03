b40450d27f67418ef89b151bba68e35d5e54cc9d
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2019-07-03 14:18:41 +0200

    fix owoner for  /var/log/mosquitto/mosquitto.log

--- docker/Mosquitto/startMosquitto.sh
+++ docker/Mosquitto/startMosquitto.sh
@@ -2,6 +2,7 @@
 
 touch /var/log/mosquitto/mosquitto.log
 chmod 664 /var/log/mosquitto/mosquitto.log
+chown mosquitto.mosquitto /var/log/mosquitto/mosquitto.log
 
 mosquitto_passwd -b /etc/mosquitto/pwfile iota ${IOTA_PASS}
 /usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf &
