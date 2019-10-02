1bfdbde1769557642abf5f8593def53e9205f6c3
Jason Fox <jason.fox@fiware.org>
2019-10-02 10:35:26 +0200

    Remove unnecessary import

--- test/unit/general/config-multi-core-test.js
+++ test/unit/general/config-multi-core-test.js
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU Affero General Public
  * License along with fiware-iotagent-lib.
- * If not, seehttp://www.gnu.org/licenses/.
+ * If not, see http://www.gnu.org/licenses/.
  *
  * For those usages not covered by the GNU Affero General Public License
  * please contact with::[contacto@tid.es]
@@ -25,8 +25,7 @@
  */
 'use strict';
 
-var iotAgentLib = require('../../../lib/fiware-iotagent-lib'),
-    config = require('../../../lib/commonConfig'),
+let config = require('../../../lib/commonConfig'),
     iotAgentConfig = {
         logLevel: 'FATAL',
         contextBroker: {
