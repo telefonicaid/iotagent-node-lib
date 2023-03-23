6e6254cf756231cea89603477959a2f6b0de042c
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2023-03-23 15:18:59 +0100

    remove plugins from test

--- test/unit/general/loglevel-api_test.js
+++ test/unit/general/loglevel-api_test.js
@@ -73,8 +73,8 @@ describe('Log level API', function () {
 
         iotAgentLib.activate(iotAgentConfig, function () {
             iotAgentLib.clearAll(function () {
-                iotAgentLib.addUpdateMiddleware(iotAgentLib.dataPlugins.attributeAlias.update);
-                iotAgentLib.addQueryMiddleware(iotAgentLib.dataPlugins.attributeAlias.query);
+                // iotAgentLib.addUpdateMiddleware(iotAgentLib.dataPlugins.attributeAlias.update);
+                // iotAgentLib.addQueryMiddleware(iotAgentLib.dataPlugins.attributeAlias.query);
                 done();
             });
         });
