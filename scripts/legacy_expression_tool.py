16ec446d7bf5bd8ddfbd13060e136e6b80b9ee1a
mapedraza <40356341+mapedraza@users.noreply.github.com>
2023-06-13 17:22:57 +0200

    Add file disclaimer

--- scripts/legacy_expression_tool.py
+++ scripts/legacy_expression_tool.py
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2023 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-iotagent-lib
+ *
+ * fiware-iotagent-lib is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * fiware-iotagent-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with fiware-iotagent-lib.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * Author by: Miguel Angel Pedraza
+ */
+
+/* eslint-disable consistent-return */
+
 from pymongo import MongoClient
 from bson import json_util, ObjectId
 import json
