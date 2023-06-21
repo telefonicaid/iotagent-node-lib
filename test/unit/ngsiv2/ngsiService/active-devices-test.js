e6f5eba78e29c50ffb56ee6322076d9d17fb0619
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2023-06-21 12:59:12 +0200

    update patch

--- test/unit/ngsiv2/ngsiService/active-devices-test.js
+++ test/unit/ngsiv2/ngsiService/active-devices-test.js
@@ -782,7 +782,7 @@ describe('NGSI-v2 - Active attributes test', function () {
             contextBrokerMock = nock('http://cbhost:1026')
                 .matchHeader('fiware-service', 'smartgondor')
                 .matchHeader('fiware-servicepath', 'gardens')
-                .post(
+                .patch(
                     '/v2/entities/light1/attrs',
                     utils.readExampleFile('./test/unit/ngsiv2/examples/contextRequests/updateContext.json')
                 )
