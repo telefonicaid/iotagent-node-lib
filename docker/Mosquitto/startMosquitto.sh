02014c9b4fed723e19ff50422d0f57c81151c11e
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2026-04-17 11:16:41 +0200

    supress warnings at startup

--- docker/Mosquitto/startMosquitto.sh
+++ docker/Mosquitto/startMosquitto.sh
@@ -1,29 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/sh
+set -eu
 
 echo "INFO: startMosquitto..."
 
-if [ "${CONGIF_FROM_ENV}" = true ] ; then
+if [ "${CONFIG_FROM_ENV}" = "true" ] ; then
     cp /etc/mosquitto/mosquitto.conf.orig /etc/mosquitto/mosquitto.conf
-    sed -i 's/log_dest file \/var\/log\/mosquitto\/mosquitto.log/log_dest stderr/g' /etc/mosquitto/mosquitto.conf
-    echo "log_timestamp true" >> /etc/mosquitto/mosquitto.conf
-    echo "log_timestamp_format %Y-%m-%dT%H:%M:%S" >> /etc/mosquitto/mosquitto.conf
-    echo 'listener 9001' >> /etc/mosquitto/mosquitto.conf
-    echo 'protocol websockets' >> /etc/mosquitto/mosquitto.conf
-    echo 'listener 1883' >> /etc/mosquitto/mosquitto.conf
-    echo 'protocol mqtt' >> /etc/mosquitto/mosquitto.conf
-    if ! [ -z "${IOTA_PASS}" ] ; then
-      # Only if IOTA_PASS is set and not empty MQTT user/pass authentication is used
-      touch /etc/mosquitto/pwfile
-      sed -i '$ i acl_file /etc/mosquitto/aclfile\npassword_file /etc/mosquitto/pwfile' /etc/mosquitto/mosquitto.conf
-      cp -f /root/aclfile /etc/mosquitto/aclfile
-      sed -i 's/user iota/user '${IOTA_USER}'/g' /etc/mosquitto/aclfile
-      mosquitto_passwd -b /etc/mosquitto/pwfile ${IOTA_USER} ${IOTA_PASS}
+
+    sed -i 's|log_dest file /var/log/mosquitto/mosquitto.log|log_dest stderr|g' /etc/mosquitto/mosquitto.conf
+
+    cat >> /etc/mosquitto/mosquitto.conf <<'EOF'
+log_timestamp true
+log_timestamp_format %Y-%m-%dT%H:%M:%S
+listener 9001
+protocol websockets
+listener 1883
+protocol mqtt
+EOF
+
+    if [ -n "${IOTA_PASS:-}" ] ; then
+        sed -i '$ i acl_file /etc/mosquitto/aclfile\npassword_file /etc/mosquitto/pwfile' /etc/mosquitto/mosquitto.conf
+
+        cp -f /root/aclfile /etc/mosquitto/aclfile
+        sed -i "s/user iota/user ${IOTA_USER}/g" /etc/mosquitto/aclfile
+
+        : > /etc/mosquitto/pwfile
+        mosquitto_passwd -b /etc/mosquitto/pwfile "${IOTA_USER}" "${IOTA_PASS}"
+
+        chown mosquitto:mosquitto /etc/mosquitto/pwfile /etc/mosquitto/aclfile
+        chmod 0700 /etc/mosquitto/pwfile /etc/mosquitto/aclfile
     fi
 fi
 
-echo "INFO: content /etc/mosquitto/mosquitto.conf: "
+echo "INFO: content /etc/mosquitto/mosquitto.conf:"
 cat /etc/mosquitto/mosquitto.conf
 
-echo "INFO: start: startMosquitto -c /etc/mosquitto/mosquitto.conf"
+echo "INFO: start: /usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf"
+exec /usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf
 
-/usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf
